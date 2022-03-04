3a05f64690a7a49d3e18dd3dc613c78d47bb24d4
Elyane <elyamehiyeva@gmail.com>
2022-03-04 10:09:43 +0400

    4-mart ucun task

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// Author : Elyane Mehiyeva
+string ay = "Avgust";
+
+
+
+switch(ay)
+{
+    case "Yanvar":        
+        Console.WriteLine(30);
+        break;
+    case "Fevral":
+        Console.WriteLine(28);
+        break;
+    case "Mart":
+        Console.WriteLine(31);
+        break;
+    case "Aprel":
+        Console.WriteLine(30);
+        break;
+    case "May":
+        Console.WriteLine(31);
+        break;
+    case "Iyun":
+        Console.WriteLine(30);
+        break;
+    case "Iyul":
+        Console.WriteLine(31);
+        break;
+    case "Avgust":
+        Console.WriteLine(31);
+        break;
+    case "Sentyabr":
+        Console.WriteLine(30);
+        break;
+    case "Oktyabr":
+        Console.WriteLine(31);
+        break;
+    case "Noyabr":
+        Console.WriteLine(30);
+        break;
+    case "Dekabr":
+        Console.WriteLine(31);
+        break;        
+    default:
+        Console.WriteLine("Xeta Bas Verdi!!!");
+        break;
+}
