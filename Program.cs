3d9cf44d86241180ad2309a5890b99dc0aafd56b
Elyane <elyamehiyeva@gmail.com>
2022-03-05 10:13:25 +0400

    5-mart

--- Program.cs
+++ Program.cs
@@ -1,4 +1,73 @@
 ﻿// Author : Elyane Mehiyeva
+/*
+int total = 0;
+int[] array = { 12,45,3,1,67,32,8,100};
+//               |  | | | |   | |   |
+//               0  1 2 3 4   5 6   7
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 3 == 0)
+    {
+        total = total + array[i];
+    }
+}
+Console.WriteLine(total);
+*/
+/*
+string text = "Elyane Mehiyeva";
+char axtarilanHerf = 'e';
+int count = 0;
+for (int t = 0; t < text.Length; t++) 
+{
+    if (axtarilanHerf == text[t])
+    {
+        count++;
+    }
+}
+if (count == 0)
+{
+    Console.WriteLine("Axtarilan herf metnde yoxdur");
+} else
+{
+    Console.WriteLine("Axtarilan herfden metnde " + count  + " dene var");
+}
+
+*/
+
+
+
+/*
+Console.WriteLine("Bir eded verilir hemin ededin ikinin quvveti olub olmadigi tapilir");
+int number = 15;
+int netice = 1;
+if (number< 1)
+{
+    Console.WriteLine("eded birden kicikdir!!!");
+}
+else
+{
+    while (netice < number)
+    {
+        netice = netice * 2;
+    }
+    if (netice > number)
+    {
+        netice = netice / 2;
+    }
+    if (netice == number)
+    {
+        Console.WriteLine("eded  ikinin quvvetidir!!!");
+    }
+    else
+    {
+        Console.WriteLine("eded ikinin quvveti deyil!!!");
+    }
+
+}
+*/
+
+/*
+ * 4 mart taski
 string ay = "Avgust";
 
 
@@ -45,3 +114,4 @@ switch(ay)
         Console.WriteLine("Xeta Bas Verdi!!!");
         break;
 }
+*/
